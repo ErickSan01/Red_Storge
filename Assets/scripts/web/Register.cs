a80ffc512e167bb3a2edd43a66672e10bbf88b48
SilverParsnip96 <39203604@uaz.edu.mx>
2023-02-02 21:31:12 -0600

    Registrar usuario funcional

--- Assets/scripts/web/Register.cs
+++ Assets/scripts/web/Register.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class Register : MonoBehaviour
 {
     public Servidor servidor;
@@ -21,11 +22,17 @@ public class Register : MonoBehaviour
     IEnumerator Registrar()
     {
         imLoading.SetActive(true);
-        string[] datos = new string[4];
+        string[] datos = new string[5];
         datos[0] = inpUsername.text;
         datos[1] = inpFullname.text;
         datos[2] = inpEmail.text;
         datos[3] = inpPass.text;
+        datos[4] = "c";
+        Debug.Log(datos[0]);
+        Debug.Log(datos[1]);
+        Debug.Log(datos[2]);
+        Debug.Log(datos[3]);
+
 
         StartCoroutine(servidor.consumirServicio("reg_usuario", datos));
         yield return new WaitForSeconds(0.5f);
