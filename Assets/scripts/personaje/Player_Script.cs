133dc3f05054804efe1bb5b76cbc223d5d8a8373
Erick Sanchez Martinez <39204127@uaz.edu.mx>
2023-03-02 22:37:10 -0600

    Mejora movimiento personaje cuando tiene una colision y sale de una colision

--- Assets/scripts/personaje/Player_Script.cs
+++ Assets/scripts/personaje/Player_Script.cs
@@ -18,6 +18,8 @@ public class Player_Script : MonoBehaviour
 
     public bool inPopUp = false;
 
+    public bool ignorarColisiones = false;
+
     // Update is called once per frame
     void Update()
     {
@@ -25,14 +27,18 @@ public class Player_Script : MonoBehaviour
             // Checa si se hace click izquerdo en el mouse
             if (Input.GetMouseButtonDown(0))
             {
-                // Obtener la posición de donde se hizo click
-                Vector3 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                clickPosition.z = 0; // Bloquea la coordenada z para que el personaje no cambie de plano
-                clickPosition.y = maxHeight; // Bloquear la coordenada y para que el personaje no suba más de lo permitido
+                if (ignorarColisiones) {
+                    ignorarColisiones = false;
+                } else {
+                    // Obtener la posición de donde se hizo click
+                    Vector3 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    clickPosition.z = 0; // Bloquea la coordenada z para que el personaje no cambie de plano
+                    clickPosition.y = maxHeight; // Bloquear la coordenada y para que el personaje no suba más de lo permitido
 
-                // Asigna el destino a la posición donde se hizo click
-                targetPosition = clickPosition;
-                isMoving = true; // El personaje se empieza a mover
+                    // Asigna el destino a la posición donde se hizo click
+                    targetPosition = clickPosition;
+                    isMoving = true; // El personaje se empieza a mover
+                }
             }
 
             Vector3 direction = Vector3.zero;
@@ -40,9 +46,9 @@ public class Player_Script : MonoBehaviour
 
             if (isMoving)
             {
-            // Calcula la distancia y la dirección de el destino
-            direction = targetPosition - transform.position;
-            float distance = direction.magnitude;
+                // Calcula la distancia y la dirección de el destino
+                direction = targetPosition - transform.position;
+                float distance = direction.magnitude;
                 // Checa si el personaje llegó al destino
                 if (distance < 0.1f)
                 {
@@ -50,36 +56,14 @@ public class Player_Script : MonoBehaviour
                 }
                 else
                 {
-                    // Se checan colisiones con otros objetos
-                    Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.1f);
-                    foreach (Collider2D collider in colliders)
-                    {
-                        if (collider.gameObject != gameObject)
-                        {
-                            isMoving = false; // El personaje se deja de mover si se detecta alguna colisión
-
-                            Action action1 = () =>{ Camera.main.backgroundColor = UnityEngine.Random.ColorHSV();};
-
-                            GetComponent<Renderer>().enabled = false;
-
-                            Popup popup = UIController.Instance.CreatePopUp();
-
-                            popup.Init(UIController.Instance.MainCanvas, "Pregunta X", "Opcion A", "Opcion B", "Opcion C", action1, this);
-                            inPopUp = true;
-                            walk = false;
-                            anim.SetBool("walk",walk);
-                        }
-                    }
-
                     // El personaje se mueve hacia el destino
-                    if(isMoving){
+                    if(isMoving && !ignorarColisiones){
                         transform.Translate(direction.normalized * moveSpeed * Time.deltaTime);
                     }
                 }
                 walk = true;
                 anim.SetBool("walk",walk);
                 // move the character towards the target position
-                Debug.Log(direction.normalized);
                 transform.Translate(direction.normalized * moveSpeed * Time.deltaTime);
             }else{
                 walk = false;
@@ -108,6 +92,27 @@ public class Player_Script : MonoBehaviour
     private void Start() {
         transform.position = new Vector3(-8.68f,-2.96f,1f);
         anim = GetComponent<Animator>();
+    }
+    
+     private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "Pergamino"){
+            isMoving = false; // El personaje se deja de mover si se detecta alguna colisión
+        
+            Action action1 = () =>{ Camera.main.backgroundColor = UnityEngine.Random.ColorHSV();};
+        
+            GetComponent<Renderer>().enabled = false;
+
+            Popup popup = UIController.Instance.CreatePopUp();
+
+            popup.Init(UIController.Instance.MainCanvas, "Pregunta X", "Opcion A", "Opcion B", "Opcion C", action1, this);
+            inPopUp = true;
+            walk = false;
+            anim.SetBool("walk",walk);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        ignorarColisiones = true;
     }
 
 
