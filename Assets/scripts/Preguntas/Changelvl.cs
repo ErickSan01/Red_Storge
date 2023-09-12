a76dca43032450076353ec94c91da180f53f6e32
Leonardo-Calderon <leo_cg2000@hotmail.com>
2023-09-11 22:05:58 -0600

    cambios

--- Assets/scripts/Preguntas/Changelvl.cs
+++ Assets/scripts/Preguntas/Changelvl.cs
@@ -6,12 +6,14 @@ using UnityEngine.SceneManagement;
 public class Changelvl : MonoBehaviour
 {
     public string nombreDelNivel; // Nombre del nivel al que deseas moverte
+    public static string textoParaSegundaEscena;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             // Cargar el siguiente nivel
+            textoParaSegundaEscena = "Texto que quieres pasar a la segunda escena";
             SceneManager.LoadScene(nombreDelNivel);
         }
     }
